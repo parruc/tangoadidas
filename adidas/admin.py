1618862d83184e312646ec01cbe06aee245df263
Matteo Parrucci <parruc@gmail.com>
2017-02-09 03:22:57 +0100

    added basic admin backend

--- adidas/admin.py
+++ adidas/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Event
+from .models import Team
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    pass
