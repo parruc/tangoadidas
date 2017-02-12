16c492b8b7039b3adbce9150d172b08a1514579d
Matteo Parrucci <parruc@gmail.com>
2017-02-12 19:40:41 +0100

    moved registration to decoorator

--- adidas/signals.py
+++ adidas/signals.py
@@ -1,12 +1,11 @@
 from adidas.models import Player
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
+@receiver(post_save, sender=User, dispatch_uid="adidas_user_creation")
 def create_player_profile(sender, instance, created, **kwargs):
     if created:
         new_profile = Player.objects.create(user=instance)
         new_profile.save()
-
-
-post_save.connect(create_player_profile, sender=User)
