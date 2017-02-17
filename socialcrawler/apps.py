c0895d1b84cce6f69d93da0e4b046f7a000f32df
Matteo Parrucci <parruc@gmail.com>
2017-02-17 10:11:25 +0100

    starting to implement social crawler cron by using uwsgi signals

--- socialcrawler/apps.py
+++ socialcrawler/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
 
+import uwsgi
+
+
+def crawl_socials(num):
+    print("crawling socials")
+
 
 class SocialcrawlerConfig(AppConfig):
     name = 'socialcrawler'
+
+    def ready(self):
+        uwsgi.register_signal(26, "worker", crawl_socials)
+        uwsgi.add_timer(26, 3600)
