48d1a0c5e306bfd000545287626f8aa8252af8ee
Matteo Parrucci <parruc@gmail.com>
2017-02-20 16:04:44 +0100

    added app config ready method and social crawling registration

--- socialcrawler/apps.py
+++ socialcrawler/apps.py
@@ -1,15 +1,22 @@
 from django.apps import AppConfig
+from django.core import management
 
 import uwsgi
 
 
-def crawl_socials(num):
-    print("crawling socials")
+def crawl_facebook():
+    management.call_command('crawl_facebook')
 
 
-class SocialcrawlerConfig(AppConfig):
+def crawl_instagram():
+    management.call_command('crawl_facebook')
+
+
+class SocialCrawlerConfig(AppConfig):
     name = 'socialcrawler'
-
     def ready(self):
-        uwsgi.register_signal(26, "worker", crawl_socials)
-        uwsgi.add_timer(26, 3600)
+        import uwsgi
+        uwsgi.register_signal(1, "worker", crawl_facebook)
+        uwsgi.add_timer(1, 30)
+        uwsgi.register_signal(2, "worker", crawl_instagram)
+        uwsgi.add_timer(2, 30)
